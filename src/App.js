4176d2cd0868d5422f83bf29498096988429ba5b
Eva Machova <eva.machova1997@gmail.com>
2024-11-23 17:53:29 +0000

    Added rel="noreferrer"

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ export default function App() {
     <div className="App">
      <h1>Weather App</h1> 
       <footer>
-        This project was coded by <a href="https://www.linkedin.com/in/eva-machova/" target="_blank">Eva Machova</a> and is open-sourced on{" "}
-        <a href="https://github.com/eva-coding/react-weather-app" target="_blank">GitHub</a>
+        This project was coded by <a href="https://www.linkedin.com/in/eva-machova/" target="_blank" rel="noreferrer">Eva Machova</a> and is open-sourced on{" "}
+        <a href="https://github.com/eva-coding/react-weather-app" target="_blank" rel="noreferrer">GitHub</a>
       </footer>
     </div>
   );
