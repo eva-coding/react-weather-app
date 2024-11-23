f1ffefd90ca63e12088d51d1daf15c97d1f8429b
Eva Machova <eva.machova1997@gmail.com>
2024-11-23 17:50:29 +0000

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      React Weather App
+     <h1>Weather App</h1> 
+      <footer>
+        This project was coded by <a href="https://www.linkedin.com/in/eva-machova/" target="_blank">Eva Machova</a> and is open-sourced on{" "}
+        <a href="https://github.com/eva-coding/react-weather-app" target="_blank">GitHub</a>
+      </footer>
     </div>
   );
 }
