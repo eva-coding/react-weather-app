6dbddff5ce67068f52430ab877ea8a5912b8f8f6
Eva Machova <eva.machova1997@gmail.com>
2024-11-23 18:11:47 +0000

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
      <h1>Weather App</h1> 
       <footer>
         This project was coded by <a href="https://www.linkedin.com/in/eva-machova/" target="_blank" rel="noreferrer">Eva Machova</a> and is open-sourced on{" "}
         <a href="https://github.com/eva-coding/react-weather-app" target="_blank" rel="noreferrer">GitHub</a>
       </footer>
+      </div>
     </div>
   );
 }
