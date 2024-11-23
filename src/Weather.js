349cd3437c03633ada2119eb3b68aa584a70079b
Eva Machova <eva.machova1997@gmail.com>
2024-11-23 20:35:46 +0000

    Added basic styling

--- src/Weather.js
+++ src/Weather.js
@@ -7,10 +7,10 @@ export default function Weather() {
         <form>
             <div className="row">
                 <div className="col-9">
-            <input type="search" placeholder="Enter a city..." className="form-control" />
+            <input type="search" placeholder="Enter a city..." className="form-control" autoFocus="on" />
             </div>
             <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input type="submit" value="Search" className="btn btn-primary w-100" />
             </div>
             </div>
         </form>
@@ -20,11 +20,16 @@ export default function Weather() {
             <li>Saturday 18:18</li>
             <li>Cloudy</li>
         </ul>
-        <div className="row">
-            <div className="col-6">
-                <img src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png" alt="cloudy" />
-                13°C
+        <div className="row mt-3">
+    <div className="col-6">
+        <div className="clearfix">
+            <img src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png" alt="cloudy" className="float-left" />
+            <div className="float-left temperature-container">
+                <span className="temperature">13</span>
+                <span className="units">°C</span>
             </div>
+    </div>
+</div>
             <div className="col-6">
             <ul>
                 <li>Precipitation: 15%</li>
