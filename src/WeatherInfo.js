b6e0b439f6edef07f2a0b47dd83e1e5236f5b549
Eva Machova <eva.machova1997@gmail.com>
2024-11-24 18:55:01 +0000

    Added temperature conversion

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherTemperature from "./WeatherTemperature";
 
 
 export default function WeatherInfo(props) {
@@ -17,8 +18,7 @@ export default function WeatherInfo(props) {
         <div className="clearfix">
             <img src={props.data.iconUrl} alt={props.data.description} className="float-left" />
             <div className="float-left temperature-container">
-                <span className="temperature">{Math.round(props.data.temperature)}</span>
-                <span className="units">°C</span>
+                <WeatherTemperature celsius={props.data.temperature} />
             </div>
     </div>
 </div>
